0148c3e5dd1f8b34b3aaa144fac633046ea965a1
Evan Kim <evanhkim09@gmail.com>
2026-04-03 21:31:39 -0400

    Auth remove pages

--- resources/js/layouts/auth/auth-simple-layout.tsx
+++ resources/js/layouts/auth/auth-simple-layout.tsx
@@ -18,7 +18,7 @@ export default function AuthSimpleLayout({
                             className="flex flex-col items-center gap-2 font-medium"
                         >
                             <div className="mb-1 flex h-9 w-9 items-center justify-center rounded-md">
-                                <AppLogoIcon className="size-9 fill-current text-[var(--foreground)] dark:text-white" />
+                                <AppLogoIcon className="size-9 dark:text-white" />
                             </div>
                             <span className="sr-only">{title}</span>
                         </Link>
